42fd96b2854640c9c6c06937467bbe49398a20c9
Synxularity <mosyam0202@gmail.com>
2026-02-09 20:50:15 +0700

    feat(header): disable settings option in dropdown

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -85,7 +85,7 @@ export const Header = () => {
                                         <User className="w-4 h-4" />
                                         Profile
                                     </button>
-                                    <button
+                                    {/* <button
                                         onClick={() => {
                                             navigate('/settings');
                                             setShowUserMenu(false);
@@ -94,7 +94,7 @@ export const Header = () => {
                                     >
                                         <Settings className="w-4 h-4" />
                                         Settings
-                                    </button>
+                                    </button> */}
                                     <hr className="my-1 border-gray-200" />
                                     <button
                                         onClick={handleLogout}
